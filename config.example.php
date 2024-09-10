0b7c46ea14c5bd3a061f0c85d5de7ed5af76c891
ProvLeon <providence.leonard@gmail.com>
2024-09-10 19:38:40 +0000

    improved the changes in the config.example.php file to prevent leak of secrets

--- config.example.php
+++ config.example.php
@@ -1,8 +1,12 @@
 <?php
+    // duplicate this file and rename it to config.php
+    // config.php
     define('BASE_URL', 'http://localhost:8000'); // Change this to your URL
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASS', '');
     define('DB_NAME', 'FinalProject'); // Change this to your database name
+
+    // Email configuration
     define('EMAIL_ACC', 'your_email_here');
     define('EMAIL_PASSWORD', 'your_app_password_here');
