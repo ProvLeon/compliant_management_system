e6d6b740c05a16f18e5c6b80cfd5e39a45aa4aef
ProvLeon <providence.leonard@gmail.com>
2024-09-10 19:40:56 +0000

    improved the changes in the config.example.php file to prevent leak of secrets

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// config.php
-define('BASE_URL', 'http://localhost:8000');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'FinalProject');
-define('EMAIL_ACC', 'providence.leonard@gmail.com');
-define('EMAIL_PASSWORD', 'wpvw wlxm nkky usup');
