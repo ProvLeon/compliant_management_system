d6863080d36fec6ea38bc0fcbc6b62460925c3fb
ProvLeon <providence.leonard@gmail.com>
2024-09-10 19:10:15 +0000

    created custom phpmailer to handle sending of email

---
+++ config.example.php
@@ -0,0 +1,8 @@
+<?php
+    define('BASE_URL', 'http://localhost:8000'); // Change this to your URL
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'FinalProject'); // Change this to your database name
+    define('EMAIL_ACC', 'your_email_here');
+    define('EMAIL_PASSWORD', 'your_app_password_here');
